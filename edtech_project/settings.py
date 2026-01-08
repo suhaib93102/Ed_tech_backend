993c5be69096c24708ad77e906660d7d769386ca
Rahul93102 <rahuljha93102@gmail.com>
2026-01-08 15:34:38 +0530

    Add Render domain to ALLOWED_HOSTS to fix DisallowedHost error

--- edtech_project/settings.py
+++ edtech_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-dev-key-change-in-producti
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1,ed-tech-backend-tzn8.onrender.com,*.onrender.com').split(',')
 
 
 # Application definition
